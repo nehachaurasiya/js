c79df26225cc09e79e137364f3ef8d577bd3055d
Neha Chaurasiya <nehachaurasiya4660@gmail.com>
2024-03-10 15:06:21 +0530

    variable

---
+++ datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+const age=21;
+console.log(3+3);
+console.log("neha");
+console.log(typeof "Neha");
+console.log(age);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ variable.js
@@ -0,0 +1,9 @@
+const Eid=12345
+let Ename="Neha";
+var Eemail="neha123@gmail.com";
+Ecity="basti"
+//console.log(Eid);
+// console.log(Ename);
+// console.log(Eemail);
+
+console.table([Eid,Ename,Eemail,Ecity]);
